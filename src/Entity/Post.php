43c12d6282394759200a797f2b50c47a777fe09b
Virgil974 <virgil.boissin@gmail.com>
2024-08-07 15:49:57 +0400

    handle post form

---
+++ src/Entity/Post.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+class Post {
+    private int $id;
+    private ?string $title = NULL;
+    private string $content;
+    private ?string $image = NULL;
+    private $user;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self 
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser($user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
